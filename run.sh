60170eec368b8982fb7728092e749ebbc7e9c5de
andrecendon <cendonandre@gmail.com>
2024-06-07 12:38:08 +0200

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Instala las dependencias
+
+pip install Flask
+pip install flask
+pip install --upgrade pip setuptools
+pip install greenlet
+pip install -r requirements.txt
+
+
+
+flask run
